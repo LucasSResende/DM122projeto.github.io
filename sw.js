11009c45cc3875e39dbd7a166cbba144e05d0fc4
LucasSResende <lucasresendesrs@gmail.com>
2024-08-07 17:02:20 -0300

    Alterando o app para instalar em dispositivos

--- sw.js
+++ sw.js
@@ -1,25 +1,65 @@
-const CACHE_NAME = 'weather-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './images/img1.svg',
-  './images/img2.svg',
-];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+    console.log(`[Service Worker] install event lifecycle!!`);
+    self.skipWaiting(); // don't wait for installation just activate it
+    event.waitUntil(installStaticAssets());
+  });
+  
+  self.addEventListener('activate', (event) => {
+    console.log(`[Service Worker] activate event lifecycle!`);
+    event.waitUntil(cacheCleanup());
+    return self.clients.claim(); // claim all tabs
+  });
+  
+  self.addEventListener('fetch', async (event) => {
+    console.log(`[Service Worker] fetch event lifecycle!`);
+    event.respondWith(cacheFirst(event.request));
+  });
+  
+  const CACHE_VERSION_KEY = 'sw-cache-v2';
+  
+  async function installStaticAssets() {
+    return caches
+      .open(CACHE_VERSION_KEY)
+      .then((cache) =>
+        cache.addAll([
+          'https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.fluid.classless.min.css',
+          'https://cdn.jsdelivr.net/npm/dexie@4.0.8/+esm',
+          './',
+          './index.html',
+          '/helpers/database.js',
+          '/helpers/install-sw.js',
+          '/helpers/repo-db.js',
+          '/images/img1.svg',
+          '/images/img2.svg',
+          './style.css',
+          './app.js',
+          './sw.js',
+        ])
+      );
+  }
+  
+  async function cacheCleanup() {
+    const cacheKeys = await caches.keys();
+    const outdatedCache = (cacheKey) => cacheKey !== CACHE_VERSION_KEY;
+    const purge = (cacheKey) => caches.delete(cacheKey);
+    cacheKeys.filter(outdatedCache).forEach(purge);
+    return true;
+  }
+  
+  async function cacheFirst(request) {
+    const cache = await caches.open(CACHE_VERSION_KEY);
+    const response = await cache.match(request);
+    if (response) {
+      return response;
+    }
+    console.log('URL not in the cache: ', request.url);
+    try {
+      const networkResponse = await fetch(request);
+      return networkResponse;
+    } catch (error) {
+      return new Response(`Network error happened: ${error}`, {
+        status: 408,
+        headers: { 'Content-Type': 'text/plain' },
+      });
+    }
+  }
