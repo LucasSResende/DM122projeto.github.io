d7144319b9d830f8653ff39103ac41933a4d3de2
LucasSResende <lucasresendesrs@gmail.com>
2024-08-07 16:28:31 -0300

    Arrumando erro de criação na tabela

--- helpers/database.js
+++ helpers/database.js
@@ -1,19 +1,8 @@
-export async function getWeatherDatabase() {
+export default async function getWeatherDatabase() {
   const { default: Dexie } = await import('https://cdn.jsdelivr.net/npm/dexie@4.0.8/+esm');
   const db = new Dexie('weatherDatabase');
   db.version(1).stores({
     weather: '&city, data'
   });
   return db;
-}
-
-export async function registerServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    try {
-      await navigator.serviceWorker.register('./sw.js');
-      console.log('[Service Worker] Registered');
-    } catch (error) {
-      console.log('[Service Worker] Registration failed', error);
-    }
-  }
 }
