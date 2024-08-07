9a0e6211c1519f29add7993191cfb12030d111ac
LucasSResende <lucasresendesrs@gmail.com>
2024-08-07 13:56:06 -0300

    Alterando visualização da tabela

--- app.js
+++ app.js
@@ -33,6 +33,15 @@ async function fetchWeatherFromAPI(city) {
   }
 }
 
+export default async function getWeatherDatabase() {
+  const { default: Dexie } = await import('https://cdn.jsdelivr.net/npm/dexie@4.0.8/+esm');
+  const db = new Dexie('weatherDatabase');
+  db.version(1).stores({
+    weather: '&city, data'
+  });
+  return db;
+}
+
 function displayWeather(city, data) {
   const temperatureInCelsius = (data.main.temp - 273.15).toFixed(2);
   weatherResult.innerHTML = `
